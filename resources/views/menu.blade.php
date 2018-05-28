f6e3942f8f10cc19798ae8e3f98c7b88ab90977a
KhiemChay <nguyendinhkhiem98@gmai.com>
2018-05-28 13:49:38 +0700

    update view menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,14 +1,12 @@
-<header>
-    <nav class="{{ $type_menu }}" id="{{ $mobile_menu }}">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 menu_col">
-                    @if (has_nav_menu('main-menu'))
-                        {!! wp_nav_menu(['theme_location' => $theme_location, 'menu_class' => 'menu-primary']) !!}
-                    @endif
-                    <div class="mobile_menu_primary"></div>
-                </div>
+<nav class="{{ $type_menu }}" id="{{ $mobile_menu }}">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 menu_col">
+                @if (has_nav_menu('main-menu'))
+                    {!! wp_nav_menu(['theme_location' => $theme_location, 'menu_class' => 'menu-primary']) !!}
+                @endif
+                <div class="mobile_menu_primary"></div>
             </div>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>
